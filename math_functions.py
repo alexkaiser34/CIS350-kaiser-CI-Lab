f6982eb3cb50786285326a41c448d61f91b3d0af
Alex Kaiser <alexkaiser@me.com>
2023-12-01 13:04:02 -0500

    Adds modulus and divide and floor functions

--- math_functions.py
+++ math_functions.py
@@ -10,6 +10,17 @@ def divide_numbers(a, b):
 def multiply_numbers(a, b):
     return a * b
 
+def divide_and_floor_numbers(a,b):
+    return a // b
+
+def modulus_numbers(a, b):
+    return a % b
+
 if __name__ == "__main__":
     print("Adding:", add_numbers(2,4))
     print("Subtracting:", subtract_numbers(9,2))
+    print("Dividing:", divide_numbers(11,2))
+    print("Multiplying:", multiply_numbers(9,2))
+    print("Divide and floor:", divide_and_floor_numbers(11,2))
+    print("Modulus:", modulus_numbers(10,3))
+
--- test_math_functions.py
+++ test_math_functions.py
@@ -10,15 +10,17 @@ def test_calc_subtraction():
     output = subtract_numbers(2, 4)
     assert output == -2
 
-
 def test_calc_multiply():
     output = multiply_numbers(2,4)
     assert output == 8
 
+def test_calc_divide_and_floor():
+    output = divide_and_floor_numbers(11,2)
+    assert output == 5
 
-# def test_calc_multiply_fail():
-#     output = multiply_numbers(2,4)
-#     assert output == 16
+def test_calc_modulus():
+    output = modulus_numbers(10,3)
+    assert output == 1 
 
 
 def test_calc_divide():
